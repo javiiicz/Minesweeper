f20f6c59705d4e1515710324eb7f9ec787204e13
Javier <106848262+javiiicz@users.noreply.github.com>
2024-07-22 17:36:47 -0600

    Number Colors

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -5,6 +5,17 @@ function Cell(props) {
     const state = props.state
     let style = "uncovered "
     let content = ""
+    let color = ""
+    const colors = {
+        1: "text-blue-800",
+        2: "text-green-700",
+        3: "text-red-600",
+        4: "text-blue-950",
+        5: "text-rose-950",
+        6: "text-cyan-700",
+        7: "text-violet-800",
+        8: "text-neutral-900"
+    }
 
 
     if (state == FLAG) {
@@ -17,13 +28,15 @@ function Cell(props) {
     } else if (state in [1,2,3,4,5,6,7,8]){
         content = state
         style += "bg-gray-400 font-extrabold"
+        color = colors[state]
+
     } else {
         style = "covered"
     }
 
     return (
         <div className={"table w-full h-full " + style}>
-            <span className="table-cell align-middle text-sm">{content}</span>
+            <span className={"table-cell align-middle text-sm " + color}>{content}</span>
         </div>
     )
 }
