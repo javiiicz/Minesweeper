6bd8203e9844b04395c8810ceb558d6a41a6dcb3
Javier <106848262+javiiicz@users.noreply.github.com>
2024-07-30 19:51:34 -0600

    Add Instructions

---
+++ src/components/Instructons.jsx
@@ -0,0 +1,18 @@
+function Instructions () {
+    return (
+        <div className="mx-[10%] pb-12">
+            <span className="font-bold">Instructions: </span>
+            <div className="pl-8">
+                <ul className="list-disc">
+                    <li>The objective of the game is to decipher where all the mines in the field are located.</li>
+                    <li>Click to reveal a cell or press space to flag it.</li>
+                    <li>Cells with numbers give the number of mines in the eight cells adjacent to them.</li>
+                    <li>You can do a "quick-reveal" by pressing space on a cell that has flags equal to its number of mines in its vicinity.</li>
+                    <li>To restart the game, click the face at the top.</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Instructions
