f1c35d01852a3db3c8a9a7aedf7aded572602ef0
Javier <106848262+javiiicz@users.noreply.github.com>
2024-07-30 21:19:54 -0600

    Add submit button div

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react"
+
 function Announcement (props) {
     let style = ""
     let content = ""
+    const score = props.score
 
     switch (props.type) {
         case 0: // None
@@ -8,7 +11,7 @@ function Announcement (props) {
             content="test"
             break;
         case 1: // Win
-            content = "Congratulations! Submit your score?"
+            content = "Congratulations!"
             break;
         case 2: // Lose
             content = "You lost, try again!"
@@ -16,7 +19,14 @@ function Announcement (props) {
     }
 
     return (
-        <div className={"p-3 flex content-center items-center text-center justify-center text-lg font-medium rounded bg-gray-300/80 w-52 backdrop-blur-[1px] " + style}>{content}</div>
+        <div className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
+            <div className={"p-3 flex flex-col gap-2 content-center items-center text-center justify-center text-lg font-medium rounded bg-gray-300/80 w-52 backdrop-blur-[1px] " + style}>
+                {content}
+                {(props.type == 1) ? 
+                    <div className="bg-gray-800 text-gray-50 p-1 px-2 rounded font-bold cursor-pointer hover:bg-blue-800 transition-all duration-500">Submit Score</div> : null
+                }
+            </div>
+        </div>
     )
 }
 
